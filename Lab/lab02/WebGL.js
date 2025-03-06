9bdcf52a023548abfa91afeb9639dabf34536935
nieves <930112ab@gmail.com>
2025-03-06 17:51:56 +0800

    done

--- Lab/lab02/WebGL.js
+++ Lab/lab02/WebGL.js
@@ -1,45 +1,46 @@
 var VSHADER_SOURCE = `
+    attribute vec4 a_Position;
+    attribute vec3 a_Color;
+    varying vec4 v_Color;
     void main(){
-        
-    }
-    `;
+        gl_Position = a_Position;
+        gl_PointSize = 10.0;
+        v_Color = vec4(a_Color, 1.0);
+    }    
+`;
 
 var FSHADER_SOURCE = `
+    precision mediump float;
+    varying vec4 v_Color;
     void main(){
-        
+        gl_FragColor = v_Color;
     }
-    `;
+`;
 
 function compileShader(gl, vShaderText, fShaderText){
-    //////Build vertex and fragment shader objects
-    var vertexShader = gl.createShader(gl.VERTEX_SHADER)
-    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER)
-    //The way to  set up shader text source
-    gl.shaderSource(vertexShader, vShaderText)
-    gl.shaderSource(fragmentShader, fShaderText)
-    //compile vertex shader
-    gl.compileShader(vertexShader)
+    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    
+    gl.shaderSource(vertexShader, vShaderText);
+    gl.shaderSource(fragmentShader, fShaderText);
+    
+    gl.compileShader(vertexShader);
     if(!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)){
-        console.log('vertex shader ereror');
-        var message = gl.getShaderInfoLog(vertexShader); 
-        console.log(message);//print shader compiling error message
+        console.log('Vertex shader error:', gl.getShaderInfoLog(vertexShader));
     }
-    //compile fragment shader
-    gl.compileShader(fragmentShader)
+    
+    gl.compileShader(fragmentShader);
     if(!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)){
-        console.log('fragment shader ereror');
-        var message = gl.getShaderInfoLog(fragmentShader);
-        console.log(message);//print shader compiling error message
+        console.log('Fragment shader error:', gl.getShaderInfoLog(fragmentShader));
     }
 
-    /////link shader to program (by a self-define function)
     var program = gl.createProgram();
     gl.attachShader(program, vertexShader);
     gl.attachShader(program, fragmentShader);
     gl.linkProgram(program);
-    //if not success, log the program info, and delete it.
+    
     if(!gl.getProgramParameter(program, gl.LINK_STATUS)){
-        alert(gl.getProgramInfoLog(program) + "");
+        console.log('Program link error:', gl.getProgramInfoLog(program));
         gl.deleteProgram(program);
     }
 
@@ -48,15 +49,13 @@ function compileShader(gl, vShaderText, fShaderText){
 
 function main(){
     var canvas = document.getElementById('webgl');
-
     var gl = canvas.getContext('webgl2');
     if(!gl){
         console.log('Failed to get the rendering context for WebGL');
-        return ;
+        return;
     }
 
     let renderProgram = compileShader(gl, VSHADER_SOURCE, FSHADER_SOURCE);
- 
     gl.useProgram(renderProgram);
 
     var n = initVertexBuffers(gl, renderProgram);
@@ -64,27 +63,35 @@ function main(){
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, n); //you are NOT allowed to change this line
+    gl.drawArrays(gl.TRIANGLES, 0, n);
 }
 
 function initVertexBuffers(gl, program){
-    var vertices = new Float32Array(
-        [ ]
-        //TODO-1: vertex and color array
-    );
-    
-    //TODO-2: how many vertices to draw?
-    //var n = ???;
-    
-    //TODO-3: create a vertex buffer
-    //TODO-4: bind buffer (gl.bindBuffer)
-    //TODO-5: bind buffer data (gl.bufferData)
+    var vertices = new Float32Array([
+         0.5, 0.5,  1.0, 1.0, 1.0,
+         -0.5, -0.5,  1.0, 1.0, 1.0,  
+         -0.5, 0.5,  1.0, 0.0, 0.0,  
 
-    //TODO-6: get reference of the attribute variable for vertex position
-    //TODO-7: layout of current vertex buffer object (gl.vertexAttribPointer)
-    //TODO-8: enable the attribute array
+         0.5, 0.5,  1.0, 1.0, 1.0,
+         -0.5, -0.5,  1.0, 1.0, 1.0,
+         0.5, -0.5, 0.0, 0.0, 1.0  
+    ]);
+    
+    var n = 6;
 
-    //TODO-9 repeat TODO-6~8 for the attribute variable to store vertex color information
+    var vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+    var FSIZE = vertices.BYTES_PER_ELEMENT;
+
+    var a_Position = gl.getAttribLocation(program, 'a_Position');
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, FSIZE * 5, 0);
+    gl.enableVertexAttribArray(a_Position);
+
+    var a_Color = gl.getAttribLocation(program, 'a_Color');
+    gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, FSIZE * 5, FSIZE * 2);
+    gl.enableVertexAttribArray(a_Color);
 
     return n;
 }
