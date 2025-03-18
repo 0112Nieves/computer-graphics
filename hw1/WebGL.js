6af5bdf1a2235756ea2980abfeed2e44e9280262
nieves <930112ab@gmail.com>
2025-03-19 00:54:04 +0800

    add: show different color

--- hw1/WebGL.js
+++ hw1/WebGL.js
@@ -4,15 +4,27 @@
 
 //shader
 var VSHADER_SOURCE = `
-
-    `;
+    attribute vec4 a_Position;
+    attribute vec3 a_Color;
+    varying vec4 v_Color;
+    void main(){
+        gl_Position = a_Position;
+        gl_PointSize = 5.0;
+        v_Color = vec4(a_Color, 1.0);
+    }
+`;
 
 var FSHADER_SOURCE = `
+    precision mediump float;
+    varying vec4 v_Color;
+    void main(){
+        gl_FragColor = v_Color;
+    }
+`;
 
-    `;
 
-
-
+var canvas;
+var gl;
 var shapeFlag = 'p'; //p: point, h: hori line: v: verti line, t: triangle, q: square, c: circle
 var colorFlag = 'r'; //r g b 
 var g_points = [];
@@ -23,18 +35,48 @@ var g_squares = [];
 var g_circles = [];
 //var ... of course you may need more variables
 
+function compileShader(gl, vShaderText, fShaderText){
+    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    
+    gl.shaderSource(vertexShader, vShaderText);
+    gl.shaderSource(fragmentShader, fShaderText);
+    
+    gl.compileShader(vertexShader);
+    if(!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)){
+        console.log('Vertex shader error:', gl.getShaderInfoLog(vertexShader));
+    }
+    
+    gl.compileShader(fragmentShader);
+    if(!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)){
+        console.log('Fragment shader error:', gl.getShaderInfoLog(fragmentShader));
+    }
+
+    var program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    
+    if(!gl.getProgramParameter(program, gl.LINK_STATUS)){
+        console.log('Program link error:', gl.getProgramInfoLog(program));
+        gl.deleteProgram(program);
+    }
+
+    return program;
+}
 
 function main(){
     //////Get the canvas context
-    var canvas = document.getElementById('webgl');
-    //var gl = canvas.getContext('webgl') || canvas.getContext('exprimental-webgl') ;
-    var gl = canvas.getContext('webgl2');
+    canvas = document.getElementById('webgl');
+    gl = canvas.getContext('webgl2');
     if(!gl){
         console.log('Failed to get the rendering context for WebGL');
         return ;
     }
 
     // compile shader and use program
+    program = compileShader(gl, VSHADER_SOURCE, FSHADER_SOURCE);
+    gl.useProgram(program);
 
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
@@ -48,10 +90,9 @@ function main(){
 
 function keydown(ev){ //you may want to define more arguments for this function
     //implment keydown event here
-
-    if(ev.key == 'r'){ //an example for user press 'r'... 
-        //......  
-    }
+    if (ev.key == 'r') colorFlag = 'r';
+    else if (ev.key == 'g') colorFlag = 'g';
+    else if (ev.key == 'b') colorFlag = 'b';
 }
 
 function click(ev){ //you may want to define more arguments for this function
@@ -63,16 +104,79 @@ function click(ev){ //you may want to define more arguments for this function
     x = ((x - rect.left) - canvas.height/2)/(canvas.height/2)
     y = (canvas.width/2 - (y - rect.top))/(canvas.height/2)
 
-    //you might want to do something here
-
+    let color;
+    if (colorFlag === 'r') color = [1.0, 0.0, 0.0];
+    else if (colorFlag === 'g') color = [0.0, 1.0, 0.0];
+    else color = [0.0, 0.0, 1.0];
+    g_points.push({ position: [x, y], color });
     //self-define draw() function
     //I suggest that you can clear the canvas
     //and redraw whole frame(canvas) after any mouse click
     draw();
+    console.log("click");
 }
 
 
 function draw(){ //you may want to define more arguments for this function
     //redraw whole canvas here
     //Note: you are only allowed to same shapes of this frame by single gl.drawArrays() call
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    let vertices = [];
+    let colors = [];
+    let numVertices = 0;
+
+    g_points.forEach(point => {
+        vertices.push(...point.position);
+        colors.push(...point.color);
+        numVertices++;
+    });
+
+    g_horiLines.forEach(line => {
+        vertices.push(...line.position);
+        colors.push(...line.color);
+        numVertices++;
+    });
+
+    g_vertiLines.forEach(line => {
+        vertices.push(...line.position);
+        colors.push(...line.color);
+        numVertices++;
+    });
+
+    g_triangles.forEach(triangle => {
+        vertices.push(...triangle.position);
+        colors.push(...triangle.color);
+        numVertices++;
+    });
+
+    g_squares.forEach(square => {
+        vertices.push(...square.position);
+        colors.push(...square.color);
+        numVertices += 6;
+    });
+
+    g_circles.forEach(circle => {
+        vertices.push(...circle.position);
+        colors.push(...circle.color);
+        numVertices++;
+    });
+
+    let vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+    let a_Position = gl.getAttribLocation(program, 'a_Position');
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(a_Position);
+
+    let colorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+
+    let a_Color = gl.getAttribLocation(program, 'a_Color');
+    gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(a_Color);
+
+    gl.drawArrays(gl.POINTS, 0, numVertices);
 }
