5b72d26b9abab6c8324c850ba7ac354f246be757
nieves <930112ab@gmail.com>
2025-05-20 22:52:31 +0800

    add: example 12

--- final_project/src/WebGL.js
+++ final_project/src/WebGL.js
@@ -1,85 +1,3 @@
-function compileShader(gl, vShaderText, fShaderText) {
-  //////Build vertex and fragment shader objects
-  var vertexShader = gl.createShader(gl.VERTEX_SHADER)
-  var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER)
-  //The way to  set up shader text source
-  gl.shaderSource(vertexShader, vShaderText)
-  gl.shaderSource(fragmentShader, fShaderText)
-  //compile vertex shader
-  gl.compileShader(vertexShader)
-  if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
-    console.log('vertex shader ereror');
-    var message = gl.getShaderInfoLog(vertexShader);
-    console.log(message);//print shader compiling error message
-  }
-  //compile fragment shader
-  gl.compileShader(fragmentShader)
-  if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-    console.log('fragment shader ereror');
-    var message = gl.getShaderInfoLog(fragmentShader);
-    console.log(message);//print shader compiling error message
-  }
-
-  /////link shader to program (by a self-define function)
-  var program = gl.createProgram();
-  gl.attachShader(program, vertexShader);
-  gl.attachShader(program, fragmentShader);
-  gl.linkProgram(program);
-  //if not success, log the program info, and delete it.
-  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-    alert(gl.getProgramInfoLog(program) + "");
-    gl.deleteProgram(program);
-  }
-
-  return program;
-}
-
-/////BEGIN:///////////////////////////////////////////////////////////////////////////////////////////////
-/////The folloing three function is for creating vertex buffer, but link to shader to user later//////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////////
-function initAttributeVariable(gl, a_attribute, buffer) {
-  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-  gl.vertexAttribPointer(a_attribute, buffer.num, buffer.type, false, 0, 0);
-  gl.enableVertexAttribArray(a_attribute);
-}
-
-function initArrayBufferForLaterUse(gl, data, num, type) {
-  // Create a buffer object
-  var buffer = gl.createBuffer();
-  if (!buffer) {
-    console.log('Failed to create the buffer object');
-    return null;
-  }
-  // Write date into the buffer object
-  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-  gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
-
-  // Store the necessary information to assign the object to the attribute variable later
-  buffer.num = num;
-  buffer.type = type;
-
-  return buffer;
-}
-
-function initVertexBufferForLaterUse(gl, vertices, normals, texCoords) {
-  var nVertices = vertices.length / 3;
-
-  var o = new Object();
-  o.vertexBuffer = initArrayBufferForLaterUse(gl, new Float32Array(vertices), 3, gl.FLOAT);
-  if (normals != null) o.normalBuffer = initArrayBufferForLaterUse(gl, new Float32Array(normals), 3, gl.FLOAT);
-  if (texCoords != null) o.texCoordBuffer = initArrayBufferForLaterUse(gl, new Float32Array(texCoords), 2, gl.FLOAT);
-  //you can have error check here
-  o.numVertices = nVertices;
-
-  gl.bindBuffer(gl.ARRAY_BUFFER, null);
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
-
-  return o;
-}
-/////END://///////////////////////////////////////////////////////////////////////////////////////////////
-/////The folloing three function is for creating vertex buffer, but link to shader to user later//////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 var mouseLastX, mouseLastY;
 var mouseDragging = false;
 var angleX = 0, angleY = 0;
@@ -88,7 +6,6 @@ var modelMatrix;
 var nVertex;
 var cameraX = 0, cameraY = 0, cameraZ = 0;
 var cameraDirX = 0, cameraDirY = 0, cameraDirZ = 1;
-var cubeObj = [];
 var cubeMapTex;
 
 async function main() {
@@ -99,17 +16,6 @@ async function main() {
     return;
   }
 
-  response = await fetch('./object/cube.obj');
-  text = await response.text();
-  obj = parseOBJ(text);
-  for (let i = 0; i < obj.geometries.length; i++) {
-    let o = initVertexBufferForLaterUse(gl,
-      obj.geometries[i].data.position,
-      obj.geometries[i].data.normal,
-      obj.geometries[i].data.texcoord);
-    cubeObj.push(o);
-  }
-
   program = compileShader(gl, VSHADER_SOURCE_ENVCUBE, FSHADER_SOURCE_ENVCUBE);
   program.a_Position = gl.getAttribLocation(program, 'a_Position');
   program.u_MvpMatrix = gl.getUniformLocation(program, 'u_MvpMatrix');
@@ -118,6 +24,10 @@ async function main() {
   // gl.useProgram(program);
   init_cubemap_program();
   // init_normal_porgram();
+
+  load_all_model();
+  draw_all();
+
   canvas.onmousedown = function (ev) { mouseDown(ev) };
   canvas.onmousemove = function (ev) { mouseMove(ev) };
   canvas.onmouseup = function (ev) { mouseUp(ev) };
@@ -162,6 +72,78 @@ function draw() {
     initAttributeVariable(gl, program.a_Position, cubeObj[i].vertexBuffer);
     gl.drawArrays(gl.TRIANGLES, 0, cubeObj[i].numVertices);
   }
+}
+
+function draw_all() {
+    var viewDir = new Vector3([cameraDirX, cameraDirY, cameraDirZ]);
+    var rotateMatrix = new Matrix4();
+    rotateMatrix.setRotate(angleX, 0, -1, 0); //for mouse rotation
+    rotateMatrix.rotate(angleY, 0, 0, -1); //for mouse rotation
+    var newViewDir = rotateMatrix.multiplyVector3(viewDir);
+
+    var vMatrix = new Matrix4();
+    var pMatrix = new Matrix4();
+    // pMatrix.setPerspective(view_size, 1, 1, 1000);
+
+    // if (third_view) {
+    //     vMatrix.lookAt(
+    //         thirdcameraX,
+    //         thirdcameraY,
+    //         thirdcameraZ,
+    //         thirdcameraX + newViewDir.elements[0],
+    //         thirdcameraY + newViewDir.elements[1],
+    //         thirdcameraZ + newViewDir.elements[2],
+    //         0,
+    //         1,
+    //         0
+    //     );
+    // } else {
+    //     vMatrix.lookAt(
+    //         firstcameraX,
+    //         firstcameraY,
+    //         firstcameraZ,
+    //         firstcameraX + newViewDir.elements[0],
+    //         firstcameraY + newViewDir.elements[1],
+    //         firstcameraZ + newViewDir.elements[2],
+    //         0,
+    //         1,
+    //         0
+    //     );
+    // }
+
+    var vpMatrix = new Matrix4();
+    vpMatrix.set(pMatrix);
+    vpMatrix.multiply(vMatrix);
+
+    set_model();
+    // gl.bindFramebuffer(gl.FRAMEBUFFER, shadowfbo);
+    // gl.viewport(0, 0, offScreenWidth, offScreenHeight);
+    // draw_shadow();
+
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+    gl.viewport(0, -275, canvas.width, canvas.width);
+    drawObject(gl, program, cubeObj[0], boardMatrix);
+    // draw_world(vMatrix, pMatrix, vpMatrix);
+
+    // draw_all_reflection_object(vpMatrix);
+}
+
+function draw_world(vMatrix, pMatrix, vpMatrix) {
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.enable(gl.DEPTH_TEST);
+
+    draw_cubemap(cubeMapTex, vMatrix, pMatrix);
+
+    draw_all_object(vpMatrix);
+}
+
+function draw_shadow() {
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.enable(gl.DEPTH_TEST);
+
+    draw_all_shadow();
 }
 
 function draw_cubemap(Tex, vMatrix, pMatrix) {
